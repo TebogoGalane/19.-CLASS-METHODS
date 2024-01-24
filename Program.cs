0ab4b4004865bf6fd2c42bf261d2ce4e42fdb3e6
Lenovo-User <Lenovo-User@DESKTOP-QJ3UI1M>
2024-01-24 17:41:43 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19.CLASS_METHODS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student(70, "john", 15);
+            Student student2 = new Student(40, "tim", 16);
+
+            Console.WriteLine(student1.isobese());
+            Console.WriteLine(student2.isobese());
+
+            Console.Read();
+
+
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19.CLASS_METHODS
+{
+    internal class Student
+    {
+        public double weight;
+        public string name;
+        public int age;
+
+        public Student(double aWeight, string aName, int aAge)
+        {
+            this.weight = aWeight;
+            this.name = aName;
+            this.age = aAge;
+        }
+        public bool isobese()
+        {
+            if (weight > 60)
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        
+        
+        
+
+     
+    }
+}
